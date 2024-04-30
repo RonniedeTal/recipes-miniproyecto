dfaa009829e0802bdda4c2107704d29c223d3950
Ronnie Vélez <ronnievelz@gmail.com>
2024-04-30 17:33:55 +0200

    nav bar y components creados

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import NavBar from './components/NavBar'
+import Footer from './components/Footer'
+import SideBar from './components/SideBar'
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+    <NavBar
+     picture="src/assets/images/foodlogo.jpg"
+     />
+
+    <SideBar />
+    <Footer />
+
+      <h1>hola</h1>
+    </>
+
+  )
+}
+
+export default App
